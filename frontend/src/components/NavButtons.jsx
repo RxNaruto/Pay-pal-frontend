88aeec2f2d0f00ba03dbdffc176aa4be9d7be96b
Rithk <rithkchaudhary86@gmail.com>
2024-02-22 22:09:36 +0530

    button element

--- frontend/src/components/NavButtons.jsx
+++ frontend/src/components/NavButtons.jsx
@@ -1,5 +1,5 @@
 export const NavButtons=({label})=>{
-    return <button className=" border border-solid rounded-2xl  text-buttonc border-buttonc w-12">
+    return <button className=" border border-solid rounded-2xl  text-buttonc border-buttonc w-14 h-6 text-sm">
         {label}
     </button>
 }
